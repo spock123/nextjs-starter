39faeae294a8f6188316530c1b1ec3d3758f890b
Lars Rye Jeppesen <lars.jeppesen@gmail.com>
2022-04-15 12:59:47 +0200

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 const nextTranslate = require('next-translate');
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   env: {},
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = nextTranslate(withBundleAnalyzer(nextConfig));
+module.exports = withPWA(nextTranslate(withBundleAnalyzer(nextConfig)));
