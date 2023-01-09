451b738c37fe10ab9ef2eec008c366e0f1dccb54
Lars Rye Jeppesen <lars.jeppesen@gmail.com>
2023-01-09 02:20:23 +0100

    next 13.1

--- next.config.js
+++ next.config.js
@@ -1,16 +1,68 @@
-const nextTranslate = require('next-translate');
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+const plugins = [];
+
+if (process.env.ANALYZE === 'true') {
+  // only load dependency if env `ANALYZE` was set
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true
+  });
+
+  plugins.push(withBundleAnalyzer);
+}
+
+plugins.push(withPWA);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true
+  }
+};
+
+module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
+
+/*
+
+const nextTranslate = require('next-translate');
+const runtimeCaching = require('next-pwa/cache');
+
+const plugins = [];
+
+// Service worker configuration
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+if (process.env.ANALYZE === 'true') {
+  // only load dependency if env `ANALYZE` was set
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true
+  });
+  plugins.push(withBundleAnalyzer);
+}
+
+plugins.push(withPWA);
+
+
+/** @type {import('next').NextConfig} */
+/*
+const nextConfig = {
+  reactStrictMode: true,
   swcMinify: true,
+  appDir: true,
   productionBrowserSourceMaps: false,
   env: {},
   pwa: {
     dest: 'public',
-    runtimeCaching
+    disable: process.env.NODE_ENV === 'development'
   },
   images: {
     domains: ['images.dog.ceo']
@@ -23,5 +75,5 @@ const nextConfig = {
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
-
 module.exports = withPWA(nextTranslate(withBundleAnalyzer(nextConfig)));
+*/
